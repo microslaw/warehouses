8b54d13e71b481415fe229130935072e5178353a
microslaw <milosz.grunwald@gmail.com>
2024-05-13 15:41:11 +0200

    add second bulk insert

--- scripts/school5bulk2.sql
+++ scripts/school5bulk2.sql
@@ -1,4 +1,7 @@
-Gabriel,Mccoy,15,A18,7:00,8:00,Physics,493,Stephanie,Lopez,5,True,Write a summary of the chapter,False
-Terri,Smith,11,B14,15:00,16:00,Sociology,324,Derrick,Phillips,2,True,Do exercises 11-15,True
-Steve,Austin,10,B14,10:00,11:00,Chemistry,379,Teresa,Charles,8,True,Write a summary of the chapter,False
-Terri,Smith,11,B14,10:00,11:00,Chemistry,379,Teresa,Charles,8,False,None,False
+USE schoolDB
+GO
+
+BULK INSERT Students FROM 'C:\projects\warehouses\dbData\students2.csv' WITH (FIELDTERMINATOR=',', rowterminator = '\n' );
+BULK INSERT Exams FROM 'C:\projects\warehouses\dbData\exams2.csv' WITH (FIELDTERMINATOR=',', rowterminator = '\n' );
+BULK INSERT Meeting FROM 'C:\projects\warehouses\dbData\meetings2.csv' WITH (FIELDTERMINATOR=',', rowterminator = '\n' );
+BULK INSERT Surveys FROM 'C:\projects\warehouses\dbData\surveys2.csv' WITH (FIELDTERMINATOR=',', rowterminator = '\n' );
